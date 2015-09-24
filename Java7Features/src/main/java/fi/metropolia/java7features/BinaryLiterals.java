7bfe53e4e4f7c53ced3984a26aa5e7ba1cdec94c
simokor <simokor@edunix.metropolia.fi>
2015-09-25 00:18:25 +0300

    MOD BinaryLiterals.java, ADD Kek.java

--- Java7Features/src/main/java/fi/metropolia/java7features/BinaryLiterals.java
+++ Java7Features/src/main/java/fi/metropolia/java7features/BinaryLiterals.java
@@ -4,6 +4,7 @@ public class BinaryLiterals {
 
 	private int binaryEight = 0b1000;
 	private int binaryHundred = 0b1100100;
+	char str[] = "hauskaa";
 
 	public int getBinaryEight() {
 		return binaryEight;
