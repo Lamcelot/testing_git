1a5d15cd0ca8d8d0051d5911f97c3cfb77e391cb
“Janne <janne.ruha@gmail.com>
2015-11-30 14:51:23 +0200

    kakkosteht

--- Java7Features/src/main/java/fi/metropolia/java7features/UusiLuokka.java
+++ Java7Features/src/main/java/fi/metropolia/java7features/UusiLuokka.java
@@ -1,5 +1,5 @@
 package fi.metropolia.java7features;
 
 public class UusiLuokka() {
-	System.out.println("Hei kaikki olen uusi, muokattu luokka!!!");
+	System.out.println("Hei olen uusi, muokattu luokka!!!");
 }
